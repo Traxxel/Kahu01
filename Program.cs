4d446a5fb30663d6bd1adaa70a65f3f166e64d38
Stefan Meyer <stefan@meyer-luenen.de>
2025-05-30 15:17:06 +0200

    Hinzugefügt: DevExpress Blazor Komponenten und Styling

--- Program.cs
+++ Program.cs
@@ -4,9 +4,16 @@ using Kahu01.Data;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Kahu01;
+using DevExpress.Blazor;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
